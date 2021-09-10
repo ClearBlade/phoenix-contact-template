7d3737b0d5978411505472033609a2182b86f3ec
Yash Jain <yjain@clearblade.com>
2021-09-10 15:25:33 -0500

    updated readOPCUATags to poll assets collection for nodeIDs at the edge

--- code/services/readOPCUATags/readOPCUATags.js
+++ code/services/readOPCUATags/readOPCUATags.js
@@ -11,15 +11,51 @@
  * @param {[id: string]} req.params
  * @param {CbServer.Resp} resp
  */
-
+/**
+ * This service runs only at the edge.
+ * It polls the assets collection every 30 seconds and looks for if the nodeIDs are updated 
+ * It queries the assets table for asset with id as the edgeID.
+ * 
+ * It triggers polling on the Opcua adapter 
+ * by publishing on the opcua/read topic that adapter listens on
+ * 
+ */
 function readOPCUATags(req, resp) {
   ClearBlade.init({ request: req });
-  var client = new MQTT.Client();
-  setInterval(processMessage, 10000);
+  if (!ClearBlade.isEdge()) {
+    log('Not an Edge..');
+    resp.success("Not an edge...");
+  }
+  const client = new MQTT.Client();
+  var node_ids = "";
 
-  function processMessage() {
+  setInterval(opcuaPoll, 30000);
+  setInterval(assetsPoll, 30000);
+
+  function assetsPoll() {
+    try {
+      const database = ClearBladeAsync.Database();
+      var edgeId = ClearBlade.edgeId();
+
+      const rawQuery = "select * from assets where id='" + edgeId + "';";
+      log(rawQuery);
+      database.query(rawQuery).then(function (res) {
+        node_ids = JSON.parse(res[0].custom_data).opcua_nodeids;
+        log("updated node_ids to: ", node_ids);
+
+      }).catch(function (rej) {
+        log("failed to query database: ", rej, rej.message);
+        resp.error(rej);
+      });
+    } catch (e) {
+      log('exception: ', e.message);
+      resp.error(e.stack);
+    }
+  }
+  function opcuaPoll() {
+    log("opcuapoll for node Ids: ", node_ids.split(','));
     client.publish("opcua/read", JSON.stringify({
-        "node_ids":["ns=5;s=Arp.Plc.Eclr/xRun_Pump1", "ns=5;s=Arp.Plc.Eclr/arrPumps[1].Statistics.DailyStarts", "ns=5;s=Arp.Plc.Eclr/arrPumps[1].Statistics.DailyRuntime"]
+      "node_ids": node_ids.split(',')
     }));
   }
 }
