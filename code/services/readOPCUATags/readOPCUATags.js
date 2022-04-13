d1cb2c5600d5e223f8f48f220f5a410c7c89daf9
Weston Baxter <wbaxman@gmail.com>
2022-04-13 09:18:47 -0500

    fix different poll rates

--- code/services/readOPCUATags/readOPCUATags.js
+++ code/services/readOPCUATags/readOPCUATags.js
@@ -13,7 +13,7 @@
  */
 /**
  * This service runs only at the edge.
- * It polls the adapter_config collection every 30 seconds and looks for changes in poll rates / nodeIDs
+ * It polls the adapter_config collection every second and looks for changes in poll rates / nodeIDs
  * It queries the adapter_config table for asset with id as the edgeID.
  *
  * It triggers polling on the Opcua adapter
@@ -21,136 +21,71 @@
  *
  */
 function readOPCUATags(req, resp) {
-  ClearBlade.init({ request: req });
-  if (!ClearBlade.isEdge()) {
-    log("Not an Edge..");
-    resp.success("Not an edge...");
-  }
-  const client = new MQTT.Client();
-  const staticPollMillis = 30000;
-  var existingTags = [];
-  var intervalIDs = [];
+	ClearBlade.init({ request: req });
+	if (!ClearBlade.isEdge()) {
+		log("Not an Edge..");
+		resp.success("Not an edge...");
+	}
+	const client = new MQTT.Client();
+	const staticPollMillis = 1000;
+	const READ_TOPIC = "opcua/read";
+	var tagTimeObj = {};
 
-  //leave as is, static
-  setInterval(adapter_configPoll, staticPollMillis);
+	setInterval(adapter_configPoll, staticPollMillis);
 
-  function adapter_configPoll() {
-    try {
-      const database = ClearBladeAsync.Database();
-      var edgeId = ClearBlade.edgeId();
+	function adapter_configPoll() {
+		try {
+			const database = ClearBladeAsync.Database();
+			const edgeId = ClearBlade.edgeId();
+			const rawQuery = "select * from adapter_config where edge_name='" + edgeId + "';";
 
-      const rawQuery =
-        "select * from adapter_config where edge_name='" + edgeId + "';";
-      log(rawQuery);
-      database
-        .query(rawQuery)
-        .then(function (res) {
-          if (res.length > 0) {
-            if (JSON.parse(res[0].adapter_settings).tags !== undefined) {
-              var newTags = JSON.parse(res[0].adapter_settings).tags;
-              log("updated newTags to: ", JSON.stringify(newTags));
-              //check if there's a change
-              if (!arraysEqual(existingTags, newTags)) {
-                existingTags = newTags;
-                var sortedTags = {};
+			database
+				.query(rawQuery)
+				.then(function (res) {
+					if (res.length > 0) {
+						if (JSON.parse(res[0].adapter_settings).tags !== undefined) {
+							var tags = JSON.parse(res[0].adapter_settings).tags;
 
-                //convert tags to object array with poll rate as key, and each
-                for (var i = 0; i < newTags.length; i++) {
-                  if (newTags[i].read_method.type === "polling") {
-                    if (
-                      !sortedTags.hasOwnProperty(newTags[i].read_method.rate)
-                    ) {
-                      sortedTags[newTags[i].read_method.rate] = [
-                        newTags[i].node_id,
-                      ];
-                    } else {
-                      sortedTags[newTags[i].read_method.rate].push(
-                        newTags[i].node_id
-                      );
-                    }
-                  }
-                }
+							var tagsObj = {};
 
-                //clear each existing interval, if there are any
-                if (intervalIDs.length > 0) {
-                  for (var intervalID in intervalIDs) {
-                    clearInterval(intervalID);
-                  }
-                }
-                //for each object key, setInterval to key
-                for (var key in sortedTags) {
-                  //setInterval for each different poll rate
-                  log(
-                    "setting interval of: " +
-                      key +
-                      " for node Ids: " +
-                      sortedTags[key]
-                  );
-                  intervalIDs.push(
-                    setInterval(opcuaPoll, key * 1000, sortedTags[key])
-                  );
-                }
-              } else {
-                log("no changes to adapter_config tags");
-              }
-            } else {
-              log("no tags in adapter settings");
-            }
-          } else {
-            log("no response from database");
-          }
-        })
-        .catch(function (rej) {
-          log("failed to query database: ", rej, rej.message);
-          resp.error(rej);
-        });
-    } catch (e) {
-      log("exception: ", e.message);
-      resp.error(e.stack);
-    }
-  }
+							//convert tags to object array with poll rate as key
+							for (var i = 0; i < tags.length; i++) {
+								if (tags[i].read_method.type === "polling") {
+									if (!tagsObj.hasOwnProperty(tags[i].read_method.rate)) {
+										tagsObj[tags[i].read_method.rate] = [tags[i].node_id];
+									} else {
+										tagsObj[tags[i].read_method.rate].push(tags[i].node_id);
+									}
+								}
+							}
 
-  function opcuaPoll(node_ids) {
-    client.publish(
-      "opcua/read",
-      JSON.stringify({
-        node_ids: node_ids,
-      })
-    );
-  }
+							var currentTime = Date.now();
 
-  function arraysEqual(a, b) {
-    if (a === b) return true;
-    if (a == null || b == null) return false;
-    if (a.length !== b.length) return false;
-    a.sort(dynamicSort(Object.keys(a[0])[0]));
-    b.sort(dynamicSort(Object.keys(b[0])[0]));
-
-    for (var i = 0; i < a.length; ++i) {
-      if (!deepEqual(a, b)) return false;
-    }
-    return true;
-  }
-
-  function deepEqual(x, y) {
-    return x && y && typeof x === "object" && typeof y === "object"
-      ? Object.keys(x).length === Object.keys(y).length &&
-          Object.keys(x).reduce(function (isEqual, key) {
-            return isEqual && deepEqual(x[key], y[key]);
-          }, true)
-      : x === y;
-  }
-
-  function dynamicSort(property) {
-    var sortOrder = 1;
-    if (property[0] === "-") {
-      sortOrder = -1;
-      property = property.substr(1);
-    }
-    return function (a, b) {
-      var result =
-        a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
-      return result * sortOrder;
-    };
-  }
+							//for each object key, check if it's been longer than its interval. if so, publish.
+							for (var tag in tagsObj) {
+								var timeLastPublished = 0;
+								if (tagTimeObj.hasOwnProperty(tag)) {
+									timeLastPublished = tagTimeObj[tag];
+								}
+								if (currentTime - timeLastPublished > tag * 1000) {
+									tagTimeObj[tag] = currentTime;
+									client.publish(READ_TOPIC, JSON.stringify({ node_ids: tagsObj[tag] }));
+								}
+							}
+						} else {
+							log("no tags in adapter settings");
+						}
+					} else {
+						log("no response from database");
+					}
+				})
+				.catch(function (rej) {
+					log("failed to query database: ", rej, rej.message);
+					resp.error(rej);
+				});
+		} catch (e) {
+			log("exception: ", e.message);
+			resp.error(e.stack);
+		}
+	}
 }
